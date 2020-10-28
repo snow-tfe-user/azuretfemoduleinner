82a5d536c5ca8b391cdeaaa67e009dcaf4b62f9e
snow-tfe-user <69755779+snow-tfe-user@users.noreply.github.com>
2020-10-28 20:25:16 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,17 @@
+variable azure_rgname {}
+
+variable azure_resource_count {}
+
+variable azure_location {}
+
+variable azure_db_subnet_id {}
+
+variable azure_db_vm_size {}
+
+variable azure_db_vm_sku {}
+
+variable azure_app_subnet_id {}
+
+variable azure_app_vm_size {}
+
+variable azure_app_vm_sku {}
